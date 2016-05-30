e15c21eb577bac63b0fdefeee78439cc51759d42
ChaosLiu <liuyixigarnett@163.com>
2016-05-30 14:49:47 +0800

    2016年 5月30日 星期一 14时49分30秒 CST
    首页字体大小和颜色相关调整

--- patient/Section/Info/InfoMain/View/InfoHeaderView.m
+++ patient/Section/Info/InfoMain/View/InfoHeaderView.m
@@ -28,6 +28,8 @@
     [self addSubview:self.titleLabel];
     
     self.moreLabel = [[UILabel alloc] init];
+    self.moreLabel.font = [UIFont systemFontOfSize:14];
+    self.moreLabel.textColor = ColorWithHexRGB(0x5a5a5a);
     [self addSubview:self.moreLabel];
     
     self.moreImage = [[UIImageView alloc] init];
@@ -36,12 +38,15 @@
     [self.titleImage mas_updateConstraints:^(MASConstraintMaker *make){
         make.leading.equalTo(self).offset(12);
         make.centerY.equalTo(self).offset(0);
-        make.width.mas_equalTo(16);
-        make.height.mas_equalTo(16);
+//        make.width.mas_equalTo(16);
+//        make.height.mas_equalTo(16);
+        make.width.mas_equalTo(18);
+        make.height.mas_equalTo(18);
     }];
     
     [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make){
-        make.leading.equalTo(self.titleImage).offset(16+10);
+//        make.leading.equalTo(self.titleImage).offset(16+10);
+        make.leading.equalTo(self.titleImage).offset(18+10);
         make.centerY.equalTo(self.titleImage).offset(0);
         make.height.mas_equalTo(13);
     }];
@@ -49,12 +54,12 @@
     [self.moreImage mas_makeConstraints:^(MASConstraintMaker *make){
         make.trailing.equalTo(self).offset(-6);
         make.centerY.equalTo(self).offset(0);
-        make.width.mas_equalTo(3);
+        make.width.mas_equalTo(5);
         make.height.mas_equalTo(9);
     }];
     
     [self.moreLabel mas_makeConstraints:^(MASConstraintMaker *make){
-        make.trailing.equalTo(self.moreImage).offset(-3-8);
+        make.trailing.equalTo(self.moreImage).offset(-5-5);
         make.centerY.equalTo(self.moreImage).offset(0);
         make.height.mas_equalTo(12);
     }];
