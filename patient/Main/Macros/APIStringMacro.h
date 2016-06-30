758768d4696b8f3e4fbe57ffa3e5fce756e87b77
ChaosLiu <liuyixigarnett@163.com>
2016-06-30 17:05:16 +0800

    2016年 6月30日 星期四 17时05分01秒 CST
    本地服务器地址更改

--- patient/Main/Macros/APIStringMacro.h
+++ patient/Main/Macros/APIStringMacro.h
@@ -12,8 +12,8 @@
 //接口名称相关
 #ifdef DEBUG
 //Debug状态下的测试API
-//#define kServerAddress     @"http://192.168.5.46:83/jiuzhekan_http/"// 内网测试地址－秦兵
-//#define kServerAddressPay   @"http://192.168.5.46:83/jiuzhekan_pay/"//外网测试地址—秦兵－支付
+//#define kServerAddress     @"http://192.168.5.52:83/jiuzhekan_http/"// 内网测试地址－秦兵
+//#define kServerAddressPay   @"http://192.168.5.52:83/jiuzhekan_pay/"//外网测试地址—秦兵－支付
 //#define kServerAddress     @"http://192.168.5.102:83/jiuzhekan_http/"// 内网测试地址－欧日广
 #define kServerAddress     @"http://101.68.79.26:84/jiuzhekan_http/"// 外网测试地址
 #define kServerAddressPay   @"http://101.68.79.26:84/jiuzhekan_pay/"//外网测试地址—支付
