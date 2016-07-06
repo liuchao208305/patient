b4feae46a452ec3b86f2abacb93be03f6d22afd8
ChaosLiu <liuyixigarnett@163.com>
2016-07-07 00:15:48 +0800

    2016年 7月 7日 星期四 00时15分24秒 CST
    创建问答列表文件

---
+++ patient/Section/Mine/Controller/MineQuestionListViewController.m
@@ -0,0 +1,13 @@
+//
+//  MineQuestionListViewController.m
+//  patient
+//
+//  Created by ChaosLiu on 16/7/7.
+//  Copyright © 2016年 Hangzhou Congbao Technology Co.,Ltd. All rights reserved.
+//
+
+#import "MineQuestionListViewController.h"
+
+@implementation MineQuestionListViewController
+
+@end
