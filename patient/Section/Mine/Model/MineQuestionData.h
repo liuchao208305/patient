fa8c480d3def4d0e1fd9ddd540b4088c1ec5b63b
ChaosLiu <liuyixigarnett@163.com>
2016-07-07 00:17:24 +0800

    2016年 7月 7日 星期四 00时17分07秒 CST
    创建问答列表数据文件

---
+++ patient/Section/Mine/Model/MineQuestionData.h
@@ -0,0 +1,13 @@
+//
+//  MineQuestionData.h
+//  patient
+//
+//  Created by ChaosLiu on 16/7/7.
+//  Copyright © 2016年 Hangzhou Congbao Technology Co.,Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MineQuestionData : NSObject
+
+@end
