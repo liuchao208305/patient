40529aecb9d55e331df0485f3389f463011f6e5e
ChaosLiu <liuyixigarnett@163.com>
2016-07-07 19:28:06 +0800

    2016年 7月 7日 星期四 19时27分50秒 CST
    通知设置界面完成

--- patient/Section/Mine/Controller/MineNotificationSettingViewController.h
+++ patient/Section/Mine/Controller/MineNotificationSettingViewController.h
@@ -12,4 +12,20 @@
 
 @property (strong,nonatomic)UIScrollView *scrollView;
 
+@property (strong,nonatomic)UIView *soundBackView;
+@property (strong,nonatomic)UILabel *soundLabel;
+@property (strong,nonatomic)UISwitch *soundSwitch;
+
+@property (strong,nonatomic)UIView *vibrationBackView;
+@property (strong,nonatomic)UILabel *vibrationLabel;
+@property (strong,nonatomic)UISwitch *vibrationSwitch;
+
+@property (strong,nonatomic)UIView *disturbBackView1;
+@property (strong,nonatomic)UILabel *disturbLabel;
+@property (strong,nonatomic)UISwitch *disturbSwitch;
+
+@property (strong,nonatomic)UIView *disturbBackView2;
+@property (strong,nonatomic)UILabel *timeLabel;
+@property (strong,nonatomic)UIButton *addButton;
+
 @end
--- patient/Section/Mine/Controller/MineNotificationSettingViewController.m
+++ patient/Section/Mine/Controller/MineNotificationSettingViewController.m
@@ -7,12 +7,14 @@
 //
 
 #import "MineNotificationSettingViewController.h"
-#import "NetworkUtil.h"
-#import "HudUtil.h"
-#import "NullUtil.h"
-#import "AlertUtil.h"
-#import "AnalyticUtil.h"
 #import "AdaptionUtil.h"
+#import "NetworkUtil.h"
+#import "AlertUtil.h"
+#import "HudUtil.h"
+#import "CommonUtil.h"
+#import "AnalyticUtil.h"
+#import "VerifyUtil.h"
+#import "DateUtil.h"
 #import "LoginViewController.h"
 
 @interface MineNotificationSettingViewController ()
@@ -76,7 +78,125 @@
 }
 
 -(void)initView{
+    self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    self.scrollView.backgroundColor = kBACKGROUND_COLOR;
+    if ([AdaptionUtil isIphoneFour]) {
+        self.scrollView.contentSize = CGSizeMake(0, 1.5*SCREEN_HEIGHT);
+    }else if ([AdaptionUtil isIphoneFive]){
+        self.scrollView.contentSize = CGSizeMake(0, 1.3*SCREEN_HEIGHT);
+    }else if ([AdaptionUtil isIphoneSix] || [AdaptionUtil isIphoneSixPlus]){
+        self.scrollView.contentSize = CGSizeMake(0, 1.2*SCREEN_HEIGHT);
+    }
+    self.scrollView.scrollEnabled = YES;
+    self.scrollView.showsVerticalScrollIndicator = YES;
+    [self initScrollSubView];
+    [self.view addSubview:self.scrollView];
+}
+
+-(void)initScrollSubView{
+    self.soundBackView = [[UIView alloc] initWithFrame:CGRectMake(0, 10, SCREEN_WIDTH, 45)];
+    self.soundBackView.backgroundColor = kWHITE_COLOR;
+    [self initSoundSubView];
+    [self.scrollView addSubview:self.soundBackView];
     
+    self.vibrationBackView = [[UIView alloc] initWithFrame:CGRectMake(0, 10+45+1, SCREEN_WIDTH, 45)];
+    self.vibrationBackView.backgroundColor = kWHITE_COLOR;
+    [self initVibrationSubView];
+    [self.scrollView addSubview:self.vibrationBackView];
+    
+    self.disturbBackView1 = [[UIView alloc] initWithFrame:CGRectMake(0, 10+45+1+45+10, SCREEN_WIDTH, 45)];
+    self.disturbBackView1.backgroundColor = kWHITE_COLOR;
+    [self initDisturbSubView1];
+    [self.scrollView addSubview:self.disturbBackView1];
+    
+    self.disturbBackView2 = [[UIView alloc] initWithFrame:CGRectMake(0, 10+45+1+45+10+45+1, SCREEN_WIDTH, 56)];
+    self.disturbBackView2.backgroundColor = kWHITE_COLOR;
+    [self initDisturbSubView2];
+    [self.scrollView addSubview:self.disturbBackView2];
+}
+
+-(void)initSoundSubView{
+    self.soundLabel = [[UILabel alloc] init];
+    self.soundLabel.text = @"声音";
+    [self.soundBackView addSubview:self.soundLabel];
+    
+    self.soundSwitch = [[UISwitch alloc] init];
+    [self.soundSwitch addTarget:self action:@selector(soundSwitchChange:) forControlEvents:UIControlEventValueChanged];
+    [self.soundBackView addSubview:self.soundSwitch];
+    
+    [self.soundLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.soundBackView).offset(20);
+        make.centerY.equalTo(self.soundBackView).offset(0);
+    }];
+    
+    [self.soundSwitch mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.trailing.equalTo(self.soundBackView).offset(-20);
+        make.centerY.equalTo(self.soundBackView).offset(0);
+    }];
+}
+
+-(void)initVibrationSubView{
+    self.vibrationLabel = [[UILabel alloc] init];
+    self.vibrationLabel.text = @"振动";
+    [self.vibrationBackView addSubview:self.vibrationLabel];
+    
+    self.vibrationSwitch = [[UISwitch alloc] init];
+    [self.vibrationSwitch addTarget:self action:@selector(vibrationSwitchChange:) forControlEvents:UIControlEventValueChanged];
+    [self.vibrationBackView addSubview:self.vibrationSwitch];
+    
+    [self.vibrationLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.vibrationBackView).offset(20);
+        make.centerY.equalTo(self.vibrationBackView).offset(0);
+    }];
+    
+    [self.vibrationSwitch mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.trailing.equalTo(self.vibrationBackView).offset(-20);
+        make.centerY.equalTo(self.vibrationBackView).offset(0);
+    }];
+}
+
+-(void)initDisturbSubView1{
+    self.disturbLabel = [[UILabel alloc] init];
+    self.disturbLabel.text = @"免打扰";
+    [self.disturbBackView1 addSubview:self.disturbLabel];
+    
+    self.disturbSwitch = [[UISwitch alloc] init];
+    [self.disturbSwitch addTarget:self action:@selector(disturbSwitchChange:) forControlEvents:UIControlEventValueChanged];
+    [self.disturbBackView1 addSubview:self.disturbSwitch];
+    
+    [self.disturbLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.disturbBackView1).offset(20);
+        make.centerY.equalTo(self.disturbBackView1).offset(0);
+    }];
+    
+    [self.disturbSwitch mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.trailing.equalTo(self.disturbBackView1).offset(-20);
+        make.centerY.equalTo(self.disturbBackView1).offset(0);
+    }];
+}
+
+-(void)initDisturbSubView2{
+    self.timeLabel = [[UILabel alloc] init];
+    self.timeLabel.text = @"22:30~07:30";
+    self.timeLabel.textColor = ColorWithHexRGB(0x909090);
+    [self.disturbBackView2 addSubview:self.timeLabel];
+    
+    self.addButton = [[UIButton alloc] init];
+    [self.addButton setImage:[UIImage imageNamed:@"mine_noti_setting_add"] forState:UIControlStateNormal];
+    [self.addButton addTarget:self action:@selector(addButtonClicked) forControlEvents:UIControlEventTouchUpInside];
+    [self.disturbBackView2 addSubview:self.addButton];
+    
+    [self.timeLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.disturbBackView2).offset(20);
+        make.centerY.equalTo(self.disturbBackView2).offset(0);
+    }];
+    
+    [self.addButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.trailing.equalTo(self.disturbBackView2).offset(-20);
+        make.centerY.equalTo(self.disturbBackView2).offset(0);
+        make.width.mas_equalTo(22);
+        make.height.mas_equalTo(22);
+    }];
 }
 
 -(void)initRecognizer{
@@ -84,6 +204,33 @@
 }
 
 #pragma mark Target Action
+-(void)soundSwitchChange:(UISwitch *)sender{
+    if (sender.isOn) {
+        DLog(@"声音开关打开");
+    }else{
+        DLog(@"声音开关关闭");
+    }
+}
+
+-(void)vibrationSwitchChange:(UISwitch *)sender{
+    if (sender.isOn) {
+        DLog(@"振动开关打开");
+    }else{
+        DLog(@"振动开关关闭");
+    }
+}
+
+-(void)disturbSwitchChange:(UISwitch *)sender{
+    if (sender.isOn) {
+        DLog(@"免打扰开关打开");
+    }else{
+        DLog(@"免打扰开关关闭");
+    }
+}
+
+-(void)addButtonClicked{
+    DLog(@"addButtonClicked");
+}
 
 #pragma mark UITextViewDelegate
 
