0aa18a986049043a65a4c0afe843445255cd3705
ChaosLiu <liuyixigarnett@163.com>
2016-07-12 17:40:06 +0800

    2016年 7月12日 星期二 17时39分43秒 CST
    预约详情进一步完善

--- patient/Section/Mine/Controller/OrderListDetailViewController.h
+++ patient/Section/Mine/Controller/OrderListDetailViewController.h
@@ -54,6 +54,37 @@
 @property (strong,nonatomic)UIView *patientLineView6;
 
 @property (strong,nonatomic)UIView *patientBackView3;
+@property (strong,nonatomic)UILabel *timeLabel;
+@property (strong,nonatomic)UILabel *shuimianLabel1;
+@property (strong,nonatomic)UILabel *shuimianLabel2;
+@property (strong,nonatomic)UIView *shuimianLineView;
+@property (strong,nonatomic)UILabel *yinshiLabel1;
+@property (strong,nonatomic)UILabel *yinshiLabel2;
+@property (strong,nonatomic)UIView *yinshiLineView;
+@property (strong,nonatomic)UILabel *yinshuiLabel1;
+@property (strong,nonatomic)UILabel *yinshuiLabel2;
+@property (strong,nonatomic)UIView *yinshuiLineView;
+@property (strong,nonatomic)UILabel *dabianLabel1;
+@property (strong,nonatomic)UILabel *dabianLabel2_1;
+@property (strong,nonatomic)UILabel *dabianLabel2_2;
+@property (strong,nonatomic)UILabel *dabianLabel2_3;
+@property (strong,nonatomic)UIImageView *dabianImageView;
+@property (strong,nonatomic)UIView *dabianLineView;
+@property (strong,nonatomic)UILabel *xiaobianLabel1;
+@property (strong,nonatomic)UILabel *xiaobianLabel2_1;
+@property (strong,nonatomic)UILabel *xiaobianLabel2_2;
+@property (strong,nonatomic)UIView *xiaobianLineView;
+@property (strong,nonatomic)UILabel *hanreLabel1;
+@property (strong,nonatomic)UILabel *hanreLabel2;
+@property (strong,nonatomic)UIView *hanreLineView;
+@property (strong,nonatomic)UILabel *tiwenLabel1;
+@property (strong,nonatomic)UILabel *tiwenLabel2;
+@property (strong,nonatomic)UIView *tiwenLineView;
+@property (strong,nonatomic)UILabel *chuhanLabel1;
+@property (strong,nonatomic)UILabel *chuhanLabel2;
+@property (strong,nonatomic)UIView *chuhanLineView;
+@property (strong,nonatomic)UILabel *zhaopianLabel1;
+@property (strong,nonatomic)UILabel *zhaopianLabel2;
 
 @property (strong,nonatomic)UIView *diagnoseBackView;
 
--- patient/Section/Mine/Controller/OrderListDetailViewController.m
+++ patient/Section/Mine/Controller/OrderListDetailViewController.m
@@ -489,7 +489,314 @@
 }
 
 -(void)initPatientSubView3{
+    self.timeLabel = [[UILabel alloc] init];
+    self.timeLabel.font = [UIFont systemFontOfSize:14];
+    self.timeLabel.textColor = ColorWithHexRGB(0x909090);
+    [self.patientBackView3 addSubview:self.timeLabel];
     
+    self.shuimianLabel1 = [[UILabel alloc] init];
+    self.shuimianLabel1.font = [UIFont systemFontOfSize:14];
+    [self.patientBackView3 addSubview:self.shuimianLabel1];
+    
+    self.shuimianLabel2 = [[UILabel alloc] init];
+    self.shuimianLabel2.font = [UIFont systemFontOfSize:13];
+    self.shuimianLabel2.textColor = ColorWithHexRGB(0x909090);
+    [self.patientBackView3 addSubview:self.shuimianLabel2];
+    
+    self.shuimianLineView = [[UIView alloc] init];
+    self.shuimianLineView.backgroundColor = kBACKGROUND_COLOR;
+    [self.patientBackView3 addSubview:self.shuimianLineView];
+    
+    self.yinshiLabel1 = [[UILabel alloc] init];
+    self.yinshiLabel1.font = [UIFont systemFontOfSize:14];
+    [self.patientBackView3 addSubview:self.yinshiLabel1];
+    
+    self.yinshiLabel2 = [[UILabel alloc] init];
+    self.yinshiLabel2.font = [UIFont systemFontOfSize:13];
+    self.yinshiLabel2.textColor = ColorWithHexRGB(0x909090);
+    [self.patientBackView3 addSubview:self.yinshiLabel2];
+    
+    self.yinshiLineView = [[UIView alloc] init];
+    self.yinshiLineView.backgroundColor = kBACKGROUND_COLOR;
+    [self.patientBackView3 addSubview:self.yinshiLineView];
+    
+    self.yinshuiLabel1 = [[UILabel alloc] init];
+    self.yinshuiLabel1.font = [UIFont systemFontOfSize:14];
+    [self.patientBackView3 addSubview:self.yinshuiLabel1];
+    
+    self.yinshuiLabel2 = [[UILabel alloc] init];
+    self.yinshuiLabel2.font = [UIFont systemFontOfSize:13];
+    self.yinshuiLabel2.textColor = ColorWithHexRGB(0x909090);
+    [self.patientBackView3 addSubview:self.yinshuiLabel2];
+    
+    self.yinshuiLineView = [[UIView alloc] init];
+    self.yinshuiLineView.backgroundColor = kBACKGROUND_COLOR;
+    [self.patientBackView3 addSubview:self.yinshuiLineView];
+    
+    self.dabianLabel1 = [[UILabel alloc] init];
+    self.dabianLabel1.font = [UIFont systemFontOfSize:14];
+    [self.patientBackView3 addSubview:self.dabianLabel1];
+    
+    self.dabianLabel2_1 = [[UILabel alloc] init];
+    self.dabianLabel2_1.font = [UIFont systemFontOfSize:13];
+    self.dabianLabel2_1.textColor = ColorWithHexRGB(0x909090);
+    [self.patientBackView3 addSubview:self.dabianLabel2_1];
+    
+    self.dabianLabel2_2 = [[UILabel alloc] init];
+    self.dabianLabel2_2.font = [UIFont systemFontOfSize:13];
+    self.dabianLabel2_2.textColor = ColorWithHexRGB(0x909090);
+    [self.patientBackView3 addSubview:self.dabianLabel2_2];
+    
+    self.dabianLabel2_3 = [[UILabel alloc] init];
+    self.dabianLabel2_3.font = [UIFont systemFontOfSize:13];
+    self.dabianLabel2_3.textColor = ColorWithHexRGB(0x909090);
+    [self.patientBackView3 addSubview:self.dabianLabel2_3];
+    
+    self.dabianImageView = [[UIImageView alloc] init];
+    [self.patientBackView3 addSubview:self.dabianImageView];
+    
+    self.dabianLineView = [[UIView alloc] init];
+    self.dabianLineView.backgroundColor = kBACKGROUND_COLOR;
+    [self.patientBackView3 addSubview:self.dabianLineView];
+    
+    self.xiaobianLabel1 = [[UILabel alloc] init];
+    self.xiaobianLabel1.font = [UIFont systemFontOfSize:14];
+    [self.patientBackView3 addSubview:self.xiaobianLabel1];
+    
+    self.xiaobianLabel2_1 = [[UILabel alloc] init];
+    self.xiaobianLabel2_1.font = [UIFont systemFontOfSize:13];
+    self.xiaobianLabel2_1.textColor = ColorWithHexRGB(0x909090);
+    [self.patientBackView3 addSubview:self.xiaobianLabel2_1];
+    
+    self.xiaobianLabel2_2 = [[UILabel alloc] init];
+    self.xiaobianLabel2_2.font = [UIFont systemFontOfSize:13];
+    self.xiaobianLabel2_2.textColor = ColorWithHexRGB(0x909090);
+    [self.patientBackView3 addSubview:self.xiaobianLabel2_2];
+    
+    self.xiaobianLineView = [[UIView alloc] init];
+    self.xiaobianLineView.backgroundColor = kBACKGROUND_COLOR;
+    [self.patientBackView3 addSubview:self.xiaobianLineView];
+    
+    self.hanreLabel1 = [[UILabel alloc] init];
+    self.hanreLabel1.font = [UIFont systemFontOfSize:14];
+    [self.patientBackView3 addSubview:self.hanreLabel1];
+    
+    self.hanreLabel2 = [[UILabel alloc] init];
+    self.hanreLabel2.font = [UIFont systemFontOfSize:13];
+    self.hanreLabel2.textColor = ColorWithHexRGB(0x909090);
+    [self.patientBackView3 addSubview:self.hanreLabel2];
+    
+    self.hanreLineView = [[UIView alloc] init];
+    self.hanreLineView.backgroundColor = kBACKGROUND_COLOR;
+    [self.patientBackView3 addSubview:self.hanreLineView];
+    
+    self.tiwenLabel1 = [[UILabel alloc] init];
+    self.tiwenLabel1.font = [UIFont systemFontOfSize:14];
+    [self.patientBackView3 addSubview:self.tiwenLabel1];
+    
+    self.tiwenLabel2 = [[UILabel alloc] init];
+    self.tiwenLabel2.font = [UIFont systemFontOfSize:13];
+    self.tiwenLabel2.textColor = ColorWithHexRGB(0x909090);
+    [self.patientBackView3 addSubview:self.tiwenLabel2];
+    
+    self.tiwenLineView = [[UIView alloc] init];
+    self.tiwenLineView.backgroundColor = kBACKGROUND_COLOR;
+    [self.patientBackView3 addSubview:self.tiwenLineView];
+    
+    self.chuhanLabel1 = [[UILabel alloc] init];
+    self.chuhanLabel1.font = [UIFont systemFontOfSize:14];
+    [self.patientBackView3 addSubview:self.chuhanLabel1];
+    
+    self.chuhanLabel2 = [[UILabel alloc] init];
+    self.chuhanLabel2.font = [UIFont systemFontOfSize:13];
+    self.chuhanLabel2.textColor = ColorWithHexRGB(0x909090);
+    [self.patientBackView3 addSubview:self.chuhanLabel2];
+    
+    self.chuhanLineView = [[UIView alloc] init];
+    self.chuhanLineView.backgroundColor = kBACKGROUND_COLOR;
+    [self.patientBackView3 addSubview:self.chuhanLineView];
+    
+    self.zhaopianLabel1 = [[UILabel alloc] init];
+    self.zhaopianLabel1.font = [UIFont systemFontOfSize:14];
+    [self.patientBackView3 addSubview:self.zhaopianLabel1];
+    
+    self.zhaopianLabel2 = [[UILabel alloc] init];
+    self.zhaopianLabel2.font = [UIFont systemFontOfSize:13];
+    self.zhaopianLabel2.textColor = ColorWithHexRGB(0x909090);
+    [self.patientBackView3 addSubview:self.zhaopianLabel2];
+    
+    [self.timeLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(12);
+        make.top.equalTo(self.patientBackView3).offset(15);
+    }];
+    
+    [self.shuimianLabel1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(12);
+        make.top.equalTo(self.timeLabel.mas_bottom).offset(12);
+    }];
+    
+    [self.shuimianLabel2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.shuimianLabel1.mas_trailing).offset(10);
+        make.centerY.equalTo(self.shuimianLabel1).offset(0);
+    }];
+    
+    [self.shuimianLineView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(0);
+        make.trailing.equalTo(self.patientBackView3).offset(0);
+        make.top.equalTo(self.shuimianLabel1.mas_bottom).offset(12);
+        make.height.mas_equalTo(1);
+    }];
+    
+    [self.yinshiLabel1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(12);
+        make.top.equalTo(self.shuimianLineView.mas_bottom).offset(12);
+    }];
+    
+    [self.yinshiLabel2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.yinshiLabel1.mas_trailing).offset(10);
+        make.centerY.equalTo(self.yinshiLabel1).offset(0);
+    }];
+    
+    [self.yinshiLineView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(0);
+        make.trailing.equalTo(self.patientBackView3).offset(0);
+        make.top.equalTo(self.yinshiLabel1.mas_bottom).offset(12);
+        make.height.mas_equalTo(1);
+    }];
+    
+    [self.yinshuiLabel1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(12);
+        make.top.equalTo(self.yinshiLineView.mas_bottom).offset(12);
+    }];
+    
+    [self.yinshuiLabel2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.yinshuiLabel1.mas_trailing).offset(10);
+        make.centerY.equalTo(self.yinshuiLabel1).offset(0);
+    }];
+    
+    [self.yinshuiLineView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(0);
+        make.trailing.equalTo(self.patientBackView3).offset(0);
+        make.top.equalTo(self.yinshuiLabel1.mas_bottom).offset(12);
+        make.height.mas_equalTo(1);
+    }];
+    
+    [self.dabianLabel1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(12);
+        make.top.equalTo(self.yinshuiLineView.mas_bottom).offset(12);
+    }];
+    
+    [self.dabianLabel2_1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.dabianLabel1.mas_trailing).offset(10);
+        make.centerY.equalTo(self.dabianLabel1).offset(0);
+    }];
+    
+    [self.dabianLabel2_2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.dabianLabel2_1).offset(0);
+        make.top.equalTo(self.dabianLabel2_1.mas_bottom).offset(10);
+    }];
+    
+    [self.dabianLabel2_3 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.dabianLabel2_2).offset(0);
+        make.top.equalTo(self.dabianLabel2_2.mas_bottom).offset(10);
+    }];
+    
+    [self.dabianImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.dabianLabel2_3.mas_trailing).offset(5);
+        make.centerY.equalTo(self.dabianLabel2_3).offset(0);
+        make.width.mas_equalTo(50);
+        make.height.mas_equalTo(20);
+    }];
+    
+    [self.dabianLineView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(0);
+        make.trailing.equalTo(self.patientBackView3).offset(0);
+        make.top.equalTo(self.dabianLabel2_3.mas_bottom).offset(12);
+        make.height.mas_equalTo(1);
+    }];
+    
+    [self.xiaobianLabel1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(12);
+        make.top.equalTo(self.dabianLineView.mas_bottom).offset(12);
+    }];
+    
+    [self.xiaobianLabel2_1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.xiaobianLabel1.mas_trailing).offset(10);
+        make.centerY.equalTo(self.xiaobianLabel1).offset(0);
+    }];
+    
+    [self.xiaobianLabel2_2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.xiaobianLabel2_1).offset(0);
+        make.top.equalTo(self.xiaobianLabel2_1.mas_bottom).offset(10);
+    }];
+    
+    [self.xiaobianLineView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(0);
+        make.trailing.equalTo(self.patientBackView3).offset(0);
+        make.top.equalTo(self.xiaobianLabel2_2.mas_bottom).offset(12);
+        make.height.mas_equalTo(1);
+    }];
+    
+    [self.hanreLabel1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(12);
+        make.top.equalTo(self.xiaobianLineView.mas_bottom).offset(12);
+    }];
+    
+    [self.hanreLabel2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.hanreLabel1.mas_trailing).offset(10);
+        make.centerY.equalTo(self.hanreLabel1).offset(0);
+    }];
+    
+    [self.hanreLineView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(0);
+        make.trailing.equalTo(self.patientBackView3).offset(0);
+        make.top.equalTo(self.hanreLabel2.mas_bottom).offset(12);
+        make.height.mas_equalTo(1);
+    }];
+    
+    [self.tiwenLabel1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(12);
+        make.top.equalTo(self.hanreLineView.mas_bottom).offset(12);
+    }];
+    
+    [self.tiwenLabel2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.tiwenLabel1.mas_trailing).offset(10);
+        make.centerY.equalTo(self.tiwenLabel1).offset(0);
+    }];
+    
+    [self.tiwenLineView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(0);
+        make.trailing.equalTo(self.patientBackView3).offset(0);
+        make.top.equalTo(self.tiwenLabel2.mas_bottom).offset(12);
+        make.height.mas_equalTo(1);
+    }];
+    
+    [self.chuhanLabel1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(12);
+        make.top.equalTo(self.tiwenLineView.mas_bottom).offset(12);
+    }];
+    
+    [self.chuhanLabel2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.chuhanLabel1.mas_trailing).offset(10);
+        make.centerY.equalTo(self.chuhanLabel1).offset(0);
+    }];
+    
+    [self.chuhanLineView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(0);
+        make.trailing.equalTo(self.patientBackView3).offset(0);
+        make.top.equalTo(self.chuhanLabel2.mas_bottom).offset(12);
+        make.height.mas_equalTo(1);
+    }];
+    
+    [self.zhaopianLabel1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.patientBackView3).offset(12);
+        make.top.equalTo(self.chuhanLineView.mas_bottom).offset(12);
+    }];
+    
+    [self.zhaopianLabel2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.zhaopianLabel1.mas_trailing).offset(10);
+        make.centerY.equalTo(self.zhaopianLabel1).offset(0);
+    }];
 }
 
 -(void)initDiagnoseSubView{
@@ -629,6 +936,44 @@
     self.patientGuominshiLabel.text = [NSString stringWithFormat:@"过敏史：%@",self.patientGuominshi];
     self.patientJiazushiLabel.text = [NSString stringWithFormat:@"家族史：%@",self.patientJiazushi];
     self.patientTestLabel.text = [NSString stringWithFormat:@"%@的体质是：%@ 偏向%@",self.patientTestTime,self.patientZhutizhi,self.patientPiantizhi];
+    
+    if ([self.inspectionTime isEqualToString:@""]) {
+        self.timeLabel.text = @"暂无";
+    }else{
+        self.timeLabel.text = [self.inspectionTime substringToIndex:10];
+    }
+    self.shuimianLabel1.text = @"睡眠：";
+//    self.shuimianLabel2.text = [self.shuimian isEqualToString:@""] ? @"无" : self.shuimian;
+    self.yinshiLabel1.text = @"饮食：";
+//    self.yinshiLabel2.text = [self.yinshi isEqualToString:@""] ? @"无" : self.yinshi;
+    self.yinshuiLabel1.text = @"饮水：";
+//    self.yinshuiLabel2.text = [self.yinshui isEqualToString:@""] ? @"无" : self.yinshui;
+    self.dabianLabel1.text = @"大便：";
+//    self.dabianLabel2_1.text = [self.dabian1 isEqualToString:@""] ? @"无" : self.dabian1;
+//    self.dabianLabel2_2.text = [self.dabian2 isEqualToString:@""] ? @"无" : self.dabian2;
+//    self.dabianLabel2_3.text = [self.dabian3 isEqualToString:@""] ? @"无" : self.dabian3;
+    //        NSString *test = @"0xb6bc16";
+    //        unsigned long red = strtoul([test UTF8String],0,0);
+    //        [cell.dabianImageView setBackgroundColor:ColorWithHexRGB(red)];
+//    self.dabianyanseString = @"";
+    
+//    if ([self.dabianyanseString isEqualToString:@""]) {
+//        [cell.dabianImageView setBackgroundColor:kWHITE_COLOR];
+//    }else{
+//        unsigned long dabian = strtoul([self.dabianyanseString UTF8String],0,0);
+//        [cell.dabianImageView setBackgroundColor:ColorWithHexRGB(dabian)];
+//    }
+    self.xiaobianLabel1.text = @"小便：";
+//    self.xiaobianLabel2_1.text = [self.xiaobian1 isEqualToString:@""] ? @"无" : self.xiaobian1;
+//    self.xiaobianLabel2_2.text = [self.xiaobian2 isEqualToString:@""] ? @"无" : self.xiaobian2;
+    self.hanreLabel1.text = @"寒热：";
+//    self.hanreLabel2.text = [self.hanre isEqualToString:@""] ? @"无" : self.hanre;
+    self.tiwenLabel1.text = @"体温：";
+//    self.tiwenLabel2.text = [self.tiwen isEqualToString:@""] ? @"无" : self.tiwen;
+    self.chuhanLabel1.text = @"出汗：";
+//    self.chuhanLabel2.text = [self.chuhan isEqualToString:@""] ? @"无" : self.chuhan;
+    self.zhaopianLabel1.text = @"照片资料：";
+//    self.zhaopianLabel2.text = [self.healthPhotoString isEqualToString:@""] ? @"无" : @"有";
 }
 
 @end
