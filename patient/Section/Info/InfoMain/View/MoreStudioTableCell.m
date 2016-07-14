63d5edb51f42cc1f3adbbe6790e5bd2beca22aae
ChaosLiu <liuyixigarnett@163.com>
2016-07-14 16:02:19 +0800

    2016年 7月14日 星期四 16时01分59秒 CST
    更多工作室列表修复

--- patient/Section/Info/InfoMain/View/MoreStudioTableCell.m
+++ patient/Section/Info/InfoMain/View/MoreStudioTableCell.m
@@ -38,12 +38,12 @@
     [self.contentView addSubview:self.label1];
     
     self.label2 = [[UILabel alloc] init];
-    self.label2.font = [UIFont systemFontOfSize:11];
+    self.label2.font = [UIFont systemFontOfSize:14];
     self.label2.textColor = ColorWithHexRGB(0x909090);
     [self.contentView addSubview:self.label2];
     
     self.label3 = [[UILabel alloc] init];
-    self.label3.font = [UIFont systemFontOfSize:12];
+    self.label3.font = [UIFont systemFontOfSize:13];
     self.label3.textColor = ColorWithHexRGB(0x646464);
     self.label3.numberOfLines = 0;
     [self.contentView addSubview:self.label3];
@@ -69,7 +69,7 @@
         make.leading.equalTo(self.label1).offset(0);
         make.trailing.equalTo(self.contentView).offset(-12);
         make.top.equalTo(self.label1.mas_bottom).offset(12);
-        make.bottom.equalTo(self.contentView).offset(-16);
+//        make.bottom.equalTo(self.contentView).offset(-16);
     }];
     
 }
