fb83a5511dd121501b9ad2feb99d805ee446f900
ChaosLiu <liuyixigarnett@163.com>
2016-07-16 15:22:32 +0800

    2016年 7月16日 星期六 15时22分08秒 CST
    疾病史自适应高度修复

--- patient/Section/Info/InfoHealth/Controller/HealthDiseaseHistoryViewController.m
+++ patient/Section/Info/InfoHealth/Controller/HealthDiseaseHistoryViewController.m
@@ -273,19 +273,23 @@
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     if (indexPath.section == 0) {
         if (self.jiwangshiHideFlag == NO) {
-            return 95;
+//            return 95;
+            return 10 + [StringUtil cellWithStr:self.jiwangshiFix fontSize:13 width:SCREEN_WIDTH-24];
         }
     }else if (indexPath.section == 1){
         if (self.shoushushiHideFlag == NO) {
-            return 95;
+//            return 95;
+            return 10 + [StringUtil cellWithStr:self.shoushushiFix fontSize:13 width:SCREEN_WIDTH-24];
         }
     }else if (indexPath.section == 2){
         if (self.guominshiHideFlag == NO) {
-            return 95;
+//            return 95;
+            return 10 + [StringUtil cellWithStr:self.guominshiFix fontSize:13 width:SCREEN_WIDTH-24];
         }
     }else if (indexPath.section == 3){
         if (self.jiazushiHideFlag == NO) {
-            return 95;
+//            return 95;
+            return 10 + [StringUtil cellWithStr:self.jiazushiFix fontSize:13 width:SCREEN_WIDTH-24];
         }
     }
     return 0;
